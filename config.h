e005e1be57e7980088199dc4e1f55e146d2f478c
Pieter Lewyllie <plewyllie@gmail.com>
2014-03-28 14:13:16 +0100

    Changed SQL IP to global accessible ip

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ class Config
 private:
     Config();
 public:
-    QString webAppHostname = "localhost";
+    QString webAppHostname = "2001:470:7973::5e96:9dff:fe8a:8447";
 };
 
 #endif // CONFIG_H
