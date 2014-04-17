3d6ef8ab33d54855b7d03fdbcc6338492d8b2fa6
Pieter Lewyllie <plewyllie@gmail.com>
2014-04-17 16:09:27 +0200

    Client&Server bonjour records working both ways

--- bonjourdiscoverer.cpp
+++ bonjourdiscoverer.cpp
@@ -80,15 +80,15 @@ void BonjourDiscoverer::bonjourSocketReadyRead() {
 QList < BonjourRecord* > BonjourDiscoverer::getAllActiveRecords() {
     QList < BonjourRecord* > list;
     QHashIterator<QString, BonjourBrowser* > i(this->availableServices);
-    qDebug() << "Got the available services";
+    //qDebug() << "Got the available services";
     //qDebug() << instance->availableServices; // attention use instance & not this here
     while (i.hasNext()) {
-        qDebug() << "Got new BonjourBrowser, building list";
+        //qDebug() << "Got new BonjourBrowser, building list";
         BonjourBrowser* brw = i.next().value();
-        qDebug() << "Appending";
+        //qDebug() << "Appending";
         list.append(brw->bonjourRecords);
     }
-    qDebug() << "Return list";
+    //qDebug() << "Return list";
     return list;
 }
 
