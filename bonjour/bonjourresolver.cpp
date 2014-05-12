b1e70fb2688c25e1c362dbcef610418428befd1c
Pieter Lewyllie <plewyllie@gmail.com>
2014-05-12 12:08:06 +0200

    test

--- bonjour/bonjourresolver.cpp
+++ bonjour/bonjourresolver.cpp
@@ -83,99 +83,111 @@ void BonjourResolver::hostInfoReady(const QHostInfo &info) {
 
     qDebug() << "Going to test v6.empty";
     if (v6.empty()) { // QHostInfo was not able to fetch ipv6
-        qDebug() << "record hostname" << info.hostName();
-        qDebug() << "local hostname" << QHostInfo::localDomainName();
-        if (info.hostName() == QHostInfo::localDomainName()) {
-            // just use ::1
-            qDebug() << "use ::1";
-            v6.append("::1");
-        } else {
-            if (!v4.empty()) { // get MAC from ARP table, send a neighbor discovery and check kernel table
-                QList<QString> macs; // list of retrieve mac addresses
-                QList<QString> ifaces; // for each mac we need to know on which interface it was
-                QProcess arp;
-                qDebug() << "checking arp table";
-                arp.start("arp -an");
-                arp.waitForReadyRead(200);
-                char buf[3000];
-                int length;
-                while ((length = arp.readLine(buf, 3000))) {
+        // check that v4 ip is not a local one
+        bool local = false;
+
+        QProcess ifconfig;
+        ifconfig.start("/sbin/ifconfig");
+        ifconfig.waitForReadyRead();
+        char buf[3000];
+        int length;
+        while ((length = ifconfig.readLine(buf, 3000))) {
+            QString curLine(buf);
+            QStringList list = curLine.split(" ", QString::SkipEmptyParts);
+            foreach (QString element, list) {
+                if (element.contains(v4.at(0))) {
+                    v6.append("::1");
+                    local = true;
+                }
+            }
+        }
+        ifconfig.close();
+
+        if (!local && !v4.empty()) { // get MAC from ARP table, send a neighbor discovery and check kernel table
+            QList<QString> macs; // list of retrieve mac addresses
+            QList<QString> ifaces; // for each mac we need to know on which interface it was
+            QProcess arp;
+            qDebug() << "checking arp table";
+            arp.start("arp -an");
+            arp.waitForReadyRead(200);
+            char buf[3000];
+            int length;
+            while ((length = arp.readLine(buf, 3000))) {
+                QString curLine(buf);
+                QStringList list = curLine.split(" ", QString::SkipEmptyParts);
+                foreach (QString ipv4, v4) {
+                    if (list.at(1).contains(ipv4)) {
+                        macs.append(list.at(3));
+#ifdef __APPLE__
+                        ifaces.append(list.at(5));
+#elif __GNUC__
+                        ifaces.append(list.at(6));
+#endif
+                    }
+                }
+            }
+            arp.close();
+            qDebug() << "arp table done, macs:" << macs;
+            for (int i = 0; i < macs.length(); i++) {
+                // send an inverse neighbor discovery icmpv6 just in case no previous contact was made: DOES NOT WORK
+                // RFC 3122 is a proposed standard and not yet supported.
+
+                // we can however, send an ICMPv6 request to ff02::1 and the right mac address
+                QProcess icmpReq;
+                QStringList icmpReqArgs;
+                icmpReqArgs.append(ifaces.at(i));
+                icmpReqArgs.append(qSql->getLocalIP());
+                icmpReqArgs.append(macs.at(i));
+                icmpReq.start(QString(HELPERPATH) + "reqIp", icmpReqArgs);
+                icmpReq.waitForFinished(500);
+
+                // read from neighbor table.
+                QThread::sleep(2); // wait two seconds for icmp response to arrive
+
+                // now check neighbor table
+                QProcess ndp;
+        #ifdef __APPLE__
+                ndp.start("ndp -an");
+                ndp.waitForReadyRead();
+                while ((length = ndp.readLine(buf, 3000))) {
                     QString curLine(buf);
                     QStringList list = curLine.split(" ", QString::SkipEmptyParts);
-                    foreach (QString ipv4, v4) {
-                        if (list.at(1).contains(ipv4)) {
-                            macs.append(list.at(3));
-#ifdef __APPLE__
-                            ifaces.append(list.at(5));
-#elif __GNUC__
-                            ifaces.append(list.at(6));
-#endif
+                    if (list.at(1) == macs.at(i)) {
+                        QString ipv6 = list.at(0);
+                        if (ipv6.startsWith("fe80")) {
+                            // remove everything after %; % included
+                            int percentIndex = ipv6.indexOf("%");
+                            ipv6.truncate(percentIndex);
                         }
+
+                        // at the same time we can add the mapping to our resolver
+                        IpResolver* r = IpResolver::getInstance();
+                        r->addMapping(ipv6, macs.at(i), ifaces.at(i));
+
+                        v6.append(ipv6);
                     }
                 }
-                arp.close();
-                qDebug() << "arp table done, macs:" << macs;
-                for (int i = 0; i < macs.length(); i++) {
-                    // send an inverse neighbor discovery icmpv6 just in case no previous contact was made: DOES NOT WORK
-                    // RFC 3122 is a proposed standard and not yet supported.
+                ndp.close();
+        #elif __GNUC__
+                ndp.start("ip -6 neigh");
+                ndp.waitForReadyRead();
+                char buf[3000];
+                int length;
+                while ((length = ndp.readLine(buf, 3000))) {
+                    QString curLine(buf);
+                    QStringList list = curLine.split(" ", QString::SkipEmptyParts);
+                    if (list.at(4) == macs.at(i)) {
+                        QString ipv6 = list.at(0);
 
-                    // we can however, send an ICMPv6 request to ff02::1 and the right mac address
-                    QProcess icmpReq;
-                    QStringList icmpReqArgs;
-                    icmpReqArgs.append(ifaces.at(i));
-                    icmpReqArgs.append(qSql->getLocalIP());
-                    icmpReqArgs.append(macs.at(i));
-                    icmpReq.start(QString(HELPERPATH) + "reqIp", icmpReqArgs);
-                    icmpReq.waitForFinished(500);
+                        // at the same time we can add the mapping to our resolver
+                        IpResolver* r = IpResolver::getInstance();
+                        r->addMapping(ipv6, macs.at(i), ifaces.at(i));
 
-                    // read from neighbor table.
-                    QThread::sleep(2); // wait two seconds for icmp response to arrive
-
-                    // now check neighbor table
-                    QProcess ndp;
-            #ifdef __APPLE__
-                    ndp.start("ndp -an");
-                    ndp.waitForReadyRead();
-                    while ((length = ndp.readLine(buf, 3000))) {
-                        QString curLine(buf);
-                        QStringList list = curLine.split(" ", QString::SkipEmptyParts);
-                        if (list.at(1) == macs.at(i)) {
-                            QString ipv6 = list.at(0);
-                            if (ipv6.startsWith("fe80")) {
-                                // remove everything after %; % included
-                                int percentIndex = ipv6.indexOf("%");
-                                ipv6.truncate(percentIndex);
-                            }
-
-                            // at the same time we can add the mapping to our resolver
-                            IpResolver* r = IpResolver::getInstance();
-                            r->addMapping(ipv6, macs.at(i), ifaces.at(i));
-
-                            v6.append(ipv6);
-                        }
+                        v6.append(ipv6);
                     }
-                    ndp.close();
-            #elif __GNUC__
-                    ndp.start("ip -6 neigh");
-                    ndp.waitForReadyRead();
-                    char buf[3000];
-                    int length;
-                    while ((length = ndp.readLine(buf, 3000))) {
-                        QString curLine(buf);
-                        QStringList list = curLine.split(" ", QString::SkipEmptyParts);
-                        if (list.at(4) == macs.at(i)) {
-                            QString ipv6 = list.at(0);
-
-                            // at the same time we can add the mapping to our resolver
-                            IpResolver* r = IpResolver::getInstance();
-                            r->addMapping(ipv6, macs.at(i), ifaces.at(i));
-
-                            v6.append(ipv6);
-                        }
-                    }
-                    ndp.close();
-            #endif
                 }
+                ndp.close();
+        #endif
             }
         }
     }
