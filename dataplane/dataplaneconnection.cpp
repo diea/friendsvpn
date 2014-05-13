47926af60684e152af56f85a7c50f4dc3f009f33
Pieter Lewyllie <plewyllie@gmail.com>
2014-05-13 11:32:30 +0200

    captured srcPort

--- dataplane/dataplaneconnection.cpp
+++ dataplane/dataplaneconnection.cpp
@@ -125,7 +125,7 @@ void DataPlaneConnection::readBuffer(const char* buf) {
             qDebug() << "accIndex " << accIndex;
             // the first 16 bits of UDP or TCP header are the src_port
             quint16* srcPort = static_cast<quint16*>(malloc(sizeof(quint16)));
-            memcpy(srcPort, packetBuf + accIndex + 1, sizeof(quint16));
+            memcpy(srcPort, packetBuf + accIndex, sizeof(quint16));
             *srcPort = ntohs(*srcPort);
 
             qDebug() << "Captured srcPort" << *srcPort;
