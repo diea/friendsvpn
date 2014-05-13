f16daeba7442c555e32ebf109ef4ac96b0f57968
Pieter Lewyllie <plewyllie@gmail.com>
2014-05-13 11:26:22 +0200

    test newSocket for linux

--- helpers/newSocket.c
+++ helpers/newSocket.c
@@ -9,6 +9,18 @@
 #include <netdb.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h> // sigaction(), sigsuspend(), sig*()
+
+char* ip6tablesRule = NULL;
+
+void sig_handler(int) {
+    /* used on linux to delete the ip6tables rule */
+    if (!ip6tablesRule) return;
+
+    ip6tablesRule[11] = 'D'; // replace append by 'D' for delete in the command
+    system(ip6tablesRule);
+    exit(0);
+}
 
 /**
  * usage: sockType ipProto port addr
@@ -22,6 +34,7 @@ int main(int argc, char** argv) {
 
     if (argc != 5) return 1;
 
+#ifdef __APPLE__
     // create socket and bind for the kernel
     int fd = socket(AF_INET6, atoi(argv[1]), atoi(argv[2]));
     if (fd < 0) {
@@ -37,7 +50,7 @@ int main(int argc, char** argv) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
-    int portno = atoi(argv[3]);
+    int portno = atoi(argv[3]); // TODO should be htons ? and maybe not needed but put it in getaddrinfo!
     ((struct sockaddr_in6*) res->ai_addr)->sin6_port = htons(portno);
 
     if (bind(fd, res->ai_addr, sizeof(struct sockaddr_in6)) < 0) {
@@ -47,6 +60,24 @@ int main(int argc, char** argv) {
         }
         return 3;
     }
+#elif __GNUC__ /* linux */
+    /* on linux the "bind" trick does not work due to bind's implementation needing a "listen"
+     * to accept connections, we will thus prevent the kernel from sending its RST using an ip6tables
+     * rule */
+    struct sigaction sa;
+    sa.sa_handler = &sig_handler;
+    sa.sa_flags = SA_RESTART;
+    sigfillset(&sa.sa_mask);
+    if ((sigaction(SIGINT, &sa, NULL) == -1) || (sigaction(SIGTERM, &sa, NULL) == -1)) {
+        fprintf(stderr, "Cannot handle SIGINT or SIGTERM!\n");
+        return 4;
+    }
+
+    ip6tablesRule = malloc(400 * sizeof(char));
+    sprintf(ip6tablesRule, "ip6tables -A OUTPUT -s %s -p tcp --sport %s --tcp-flags RST RST -j DROP", argv[4], argv[3]);
+    system(ip6tablesRule);
+
+#endif
     fprintf(stderr, "all is well!\n");
 
     getchar(); // press any key to quit
