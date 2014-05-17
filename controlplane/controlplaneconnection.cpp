b5dab34b3d24e4860e0fea176f551f2cca0b6a30
Pieter Lewyllie <plewyllie@gmail.com>
2014-05-17 23:20:14 +0200

    weird

--- controlplane/controlplaneconnection.cpp
+++ controlplane/controlplaneconnection.cpp
@@ -128,6 +128,7 @@ void ControlPlaneConnection::readBuffer(const char* buf, int len) {
         if (!headerLength) {
             qDebug() << "this is packet when header 0";
             qDebug() << buf + bufferPosition;
+            return;
         }
 
         len -= headerLength + 4; // 4 count for the \r\n\r\n
