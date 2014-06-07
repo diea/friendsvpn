18a53327889ed3059bf62c333c3aec887622b880
Pieter Lewyllie <plewyllie@gmail.com>
2014-06-07 10:23:48 +0200

    no custom log

--- main.cpp
+++ main.cpp
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-#if 1
+#if 0
 #ifndef QT_NO_DEBUG_OUTPUT /* used to log timestamps test */
     QString fileName = "friendsvpn.log";
     QFile *log = new QFile(fileName);
