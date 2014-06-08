6098f39cce8291561d71fa64de5fc6f396bdcd2f
Pieter Lewyllie <plewyllie@gmail.com>
2014-06-08 11:02:47 +0200

    retry

--- main.cpp
+++ main.cpp
@@ -9,6 +9,54 @@
 #if QT_VERSION >= 0x50000 
 #include <QtConcurrent>
 #endif
+#include <ncurses.h>
+
+/* define "getch" for use on linux with ncurses */
+/* code from http://stackoverflow.com/questions/7469139/what-is-equivalent-to-getch-getche-in-linux */
+#ifndef CURSES
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios old, neww;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  neww = old; /* make new settings same as old settings */
+  neww.c_lflag &= ~ICANON; /* disable buffered i/o */
+  neww.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &neww); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+  return getch_(1);
+}
+#endif
 
 /* used for test */
 #include "proxyserver.h"
