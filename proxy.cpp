deff74da827a531eff39e1ef170e33ac10d90e5b
Pieter Lewyllie <plewyllie@gmail.com>
2014-06-12 16:47:05 +0200

    makes more sense now

--- proxy.cpp
+++ proxy.cpp
@@ -340,7 +340,7 @@ void Proxy::run_pcap(const char* dstIp) {
                     UnixSignalHandler::termSignalHandler(0);
                 }
                 qDebug() << "Could not bind on port " << listenPort << "going to use " << QString::number(port);
-            } else if (bindSocket.exitCode() == EBADF) {
+            } else if (bindSocket.exitCode() == EADDRINUSE) {
                 qDebug() << "Ip" << listenIp << "and port" << port << "already bound";
                 bound = true; // happens when some advertise two services on same port
             }
