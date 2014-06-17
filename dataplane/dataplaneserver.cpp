58dc41d2d44bada790892b8e40bd127ef590532c
Pieter Lewyllie <plewyllie@gmail.com>
2014-06-17 20:27:31 +0200

    dtlsv1

--- dataplane/dataplaneserver.cpp
+++ dataplane/dataplaneserver.cpp
@@ -30,8 +30,8 @@ DataPlaneServer* DataPlaneServer::getInstance(QObject* parent) {
 void DataPlaneServer::start() {
     server_addr.s6.sin6_family = AF_INET6;
     // we listen on public IP, which is the one stored in the DB.
-    struct in6_addr servIp;
-    inet_pton(AF_INET6, qSql->getLocalIP().toUtf8().data(), &servIp) ;
+    /*struct in6_addr servIp;
+    inet_pton(AF_INET6, qSql->getLocalIP().toUtf8().data(), &servIp) ;*/
     server_addr.s6.sin6_addr = in6addr_any; // servIp;
     server_addr.s6.sin6_port = htons(DATAPLANEPORT);
 
